d013331a078fac68e213cdaa591c9e4732c7d237
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-09-02 18:14:18 +0600

    created api to get rooms data based on the query parameters

--- index.js
+++ index.js
@@ -33,6 +33,70 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/rooms/filter', async (req, res) => {
+      const placeType = req.query.placeType;
+      const propertyType = req.query.propertyType.split(',')
+      const priceRange = req.query.priceRange.split(',')
+      const minPrice = parseFloat(priceRange[0])
+      const maxPrice = parseFloat(priceRange[1])
+      const beds = parseInt(req.query.beds)
+      const bedrooms = parseInt(req.query.bedrooms)
+
+      const rooms = await roomsCollection.find({}).toArray()
+
+      let filteredRooms;
+
+      if (placeType === "All types") {
+        const averagePrice = await roomsCollection.aggregate([
+          {
+            $group: {
+              _id: null,
+              averagePrice: { $avg: '$price' }
+            }
+          }
+        ]).toArray()
+
+        filteredRooms = rooms?.filter(room => room.price <= averagePrice[0].averagePrice)
+      }
+      else {
+        const averagePrice = await roomsCollection.aggregate([
+          {
+            $match: {
+              $or: [
+                { category: placeType },
+                { propertyType: placeType }
+              ]
+            }
+          },
+          {
+            $group: {
+              _id: null,
+              averagePrice: { $avg: '$price' }
+            }
+          }
+        ]).toArray()
+
+        filteredRooms = rooms?.filter(room => room.price <= averagePrice[0].averagePrice)
+      }
+
+      filteredRooms = filteredRooms.filter(room => room.price >= minPrice && room.price <= maxPrice)
+
+      if (beds) {
+        filteredRooms = filteredRooms?.filter(room => room.bed === beds);
+      }
+
+      if (bedrooms) {
+        filteredRooms = filteredRooms?.filter(room => room.bedrooms === bedrooms);
+      }
+
+      if (!propertyType.includes('')) {
+        filteredRooms = filteredRooms?.filter(room => propertyType.includes(room.propertyType))
+      }
+
+      res.send(filteredRooms)
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
